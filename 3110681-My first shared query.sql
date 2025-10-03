e8282b0572591db5bd9aa4fcfb8e147b51b80b2a
Zahra Rahimy <rahimyzahra@gmail.com>
2025-10-03 13:27:43 +0000

    Update query `My first shared query`

--- 3110681-My first shared query.sql
+++ 3110681-My first shared query.sql
@@ -14,6 +14,6 @@ CREATE TABLE student(
 
  select * from student;
  
- INSERT into student VALUES(2, " Kate", "Biologi");
+ INSERT into student VALUES(2, NULL, "Biologi");
 
  insert into student(student_id, name) values(3, "Jack");
