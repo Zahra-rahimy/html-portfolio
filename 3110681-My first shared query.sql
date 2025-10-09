2cdaae4833ba99176f9bb814c5eba9941e1b6d56
Zahra Rahimy <rahimyzahra@gmail.com>
2025-10-09 09:55:19 +0000

    Update query `My first shared query`

--- 3110681-My first shared query.sql
+++ 3110681-My first shared query.sql
@@ -35,6 +35,6 @@ alter table employee
      client_id int primary key,
      client_name varchar(40),
      branch_id int,
-     foreign key(branch_id) references branch(branch_id) on delete set 
+     foreign key(branch_id) references branch(branch_id) on delete set NULL
     
- )
+ );
