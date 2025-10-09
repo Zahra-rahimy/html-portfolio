14725f1c0695914b8f70cf31af537aa7b6abe246
Zahra Rahimy <rahimyzahra@gmail.com>
2025-10-09 09:42:48 +0000

    Update query `My first shared query`

--- 3110681-My first shared query.sql
+++ 3110681-My first shared query.sql
@@ -15,5 +15,6 @@ CREATE TABLE employee(
      branch_id int primary key,
      branch_name varchar(40),
      mgr_id int,
-     mgr_start_date DATE()
+     mgr_start_date DATE,
+     foreign key
  )
