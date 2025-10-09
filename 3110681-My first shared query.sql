efeb0a1021f89c3f3e6f5a89fc38291c66e67780
Zahra Rahimy <rahimyzahra@gmail.com>
2025-10-09 12:55:02 +0000

    Update query `My first shared query`

--- 3110681-My first shared query.sql
+++ 3110681-My first shared query.sql
@@ -60,7 +60,7 @@ alter table employee
 
  insert into employee values (102, 'Michael',' Scott','1964-03-15', 'M',75000, 100, null);
 
-insert into branch values(2, 'Scranton', 102, '1-04-06');
+insert into branch values(2, 'Scranton', 102, '1992-04-06');
 
 update employee 
 set branch_id = 1
